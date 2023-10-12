0b1cbc2bbc8f0d7c22af4150e0284ae711951d6e
Goran Jaić <goran.jaic@apis-it.hr>
2023-10-12 21:01:03 +0200

    actuator

--- src/main/java/hr/apisit/energentmvc/config/SecurityConfig.java
+++ src/main/java/hr/apisit/energentmvc/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                 .requestMatchers(requestMatcherBuilder.pattern("/home")).permitAll()
                 .requestMatchers(requestMatcherBuilder.pattern("/register")).permitAll()
                 .requestMatchers(requestMatcherBuilder.pattern("/register/**")).permitAll()
+                .requestMatchers(requestMatcherBuilder.pattern("/actuator/**")).permitAll()
                 .requestMatchers(requestMatcherBuilder.pattern(HttpMethod.DELETE, "/users/**" )).hasAuthority("ADMIN")
 //                .requestMatchers(requestMatcherBuilder.pattern(HttpMethod.POST, "/**" )).hasRole("ADMIN")
         ).formLogin((form) -> form
